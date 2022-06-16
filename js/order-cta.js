e6b7e5a7f10937e6b7142c6071bc23e50299f4ce
dyddyd22 <mythwing1@gmail.com>
2022-06-16 15:50:27 +0900

    ♻️  Extract event handles

--- js/order-cta.js
+++ js/order-cta.js
@@ -1,16 +1,18 @@
 const orderCta = document.querySelector('.order-cta')
 const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children
-// const orderCtaBookmarkButton = orderCta.children[0]
-// const orderCtaBuyButton = orderCta.children[1]
 const orderModal = document.querySelector('.order-form-modal')
 const orderModalOverlay = document.querySelector('.overlay')
 
-orderCtaBuyButton.addEventListener('click', function () {
+function openOrderModal() {
   orderModal.classList.add('is-open')
   orderModalOverlay.classList.add('is-active')
-})
+}
 
-orderModalOverlay.addEventListener('click', function () {
+orderCtaBuyButton.addEventListener('click', openOrderModal)
+
+function closeOrderModal() {
   orderModal.classList.remove('is-open')
   orderModalOverlay.classList.remove('is-active')
-})
+}
+
+orderModalOverlay.addEventListener('click', closeOrderModal)
